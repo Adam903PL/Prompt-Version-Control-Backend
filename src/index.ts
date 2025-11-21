57f09420f3c5e9d1ab9d25ae26e38193d45340b5
Adam Pukaluk <u46_adapuk_lbn@technischools.com>
2025-11-21 14:13:26 +0100

    chore(husky): setup husky

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express + TypeScript!");
+app.get('/', (req, res) => {
+  res.send('Hello from Express + TypeScript!');
+});
+app.get('/', (req, res) => {
+  res.send('Hello from Express + TypeScript!');
 });
 
 app.listen(port, () => {
